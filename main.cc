6ed3d23939066ab1970f7e36544a53b0316b97e2
rainfall64 <42905578+rainfall64@users.noreply.github.com>
2024-05-30 00:34:58 +0530

    Initial Commit.

---
+++ main.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+    if ( argc == 2 )
+    {
+        // TODO: Implement open a folder / file
+    }
+
+    std::cout << __cplusplus;
+
+    return 0;
+}
