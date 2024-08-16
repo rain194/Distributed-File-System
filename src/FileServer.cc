535f8382a54fea0a500d95ecfb4f3044943c8492
rainfall64 <42905578+rainfall64@users.noreply.github.com>
2024-08-17 00:45:03 +0530

    Added FileServer.cc file.

---
+++ src/FileServer.cc
@@ -0,0 +1,30 @@
+// File: src/FileServer.cc
+#include "FileServer.hh"
+
+FileServer::FileServer()
+{
+    // Constructor implementation
+}
+
+FileServer::~FileServer()
+{
+    // TODO: Destructor implementation
+}
+
+void FileServer::start()
+{
+    std::cout << "File server started." << std::endl;
+    // TODO: Initialize server resource here
+}
+
+void FileServer::stop()
+{
+    std::cout << "File server stopped." << std::endl;
+    // TODO: Clean up resources here
+}
+
+void FileServer::status() const
+{
+    std::cout << "File server is running." << std::endl;
+    // TODO: Actual status check logic here
+}
